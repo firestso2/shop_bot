4b4b4cdd97c4c53872b2a26bcbe11764e8f7138d
firestso2 <firestso2@gmail.com>
2026-04-12 15:44:09 +0300

    config.py

--- config.py
+++ config.py
@@ -20,10 +20,10 @@ REDIS_DSN     = "rediss://default:gQAAAAAAASdZAAIncDEzNTUxN2NmY2U2ZDQ0Y2Y1YTExMD
 FSM_TTL_SEC   = 1800   # 30 минут — после этого незавершённая сессия сбрасывается
 
 # ── Webhook ───────────────────────────────────────────────────────────────────
-WEBHOOK_BASE_URL  = "https://bot-1775993557-9173-skammm.bothost.tech"   # публичный домен
+WEBHOOK_BASE_URL  = "https://ambient777.bothost.ru"   # публичный домен
 WEBHOOK_BOT_PATH  = "/bot/webhook"              # путь для Telegram
 WEBHOOK_HOST      = "0.0.0.0"
-WEBHOOK_PORT      = 8443                        # порт, который слушает aiohttp
+WEBHOOK_PORT      = 8000                        # порт, который слушает aiohttp
 
 # ── Платёжные системы ────────────────────────────────────────────────────────
 CRYPTOBOT_TOKEN   = "554526:AA3lwCzWXKNkEvRNIqoIjm4kIp9JKcWZuJV"
